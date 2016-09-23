61111f63f8189f95d88b216f393c48f7a18f30fe
Demotive <mat@demotive.com>
2016-09-23 15:17:05 +0100

    Express skeleton

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
-var express = require('express');
-var app = express();
+var path = require('path')
+var express = require('express')
+var favicon = require('serve-favicon')
+var nunjucks = require('nunjucks')
+var routes = require('./app/routes.js')
+var bodyParser = require('body-parser')
+var utils = require('./lib/utils.js')
+var config = require('./app/config.js')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var app = express()
+
+// Grab environment variables specified in Procfile or as Heroku config vars
+var username = process.env.USERNAME
+var password = process.env.PASSWORD
+var env = process.env.NODE_ENV || 'development'
+var useAuth = process.env.USE_AUTH || config.useAuth
+var useHttps = process.env.USE_HTTPS || config.useHttps
+
+env = env.toLowerCase()
+useAuth = useAuth.toLowerCase()
+useHttps = useHttps.toLowerCase()
+
+// Authenticate against the environment-provided credentials, if running
+// the app in production (Heroku, effectively)
+if (env === 'production' && useAuth === 'true') {
+  app.use(utils.basicAuth(username, password))
+}
+
+// Application settings
+app.set('view engine', 'html')
+app.use('/', routes);
+
+nunjucks.configure('./app/views', {
+    autoescape: true,
+    express: app
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// Force HTTPs on production connections
+if (env === 'production' && useHttps === 'true') {
+  app.use(utils.forceHttps)
+}
+
+// Discourage crawlers
+app.get('/robots.txt', function (req, res) {
+  res.type('text/plain')
+  res.send('User-agent: *\nDisallow: /')
+})
+
+// start the app
+utils.findAvailablePort(app, function (port) {
+  console.log('Listening on port ' + port + '   url: http://localhost:' + port)
+  app.listen(port)
+})
