b4f1fa95dd700808825ad82327be7e4c1fc5ca99
Demotive <mat@demotive.com>
2016-12-22 11:05:52 +0000

    Initial setup of /vision_v1

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var v1_1 = require('./app/routes/v1.1');
 var mvp_v1 = require('./app/routes/mvp.v1');
 var mvp_v1_1 = require('./app/routes/mvp.v1.1');
 var mvp_v1_2 = require('./app/routes/mvp.v1.2');
+var vision_v1 = require('./app/routes/vision.v1');
 
 var emails = require('./app/routes/emails');
 
@@ -104,6 +105,8 @@ app.use('/v1.1', v1_1);
 app.use('/mvp-v1', mvp_v1);
 app.use('/mvp-v1.1', mvp_v1_1);
 app.use('/mvp-v1.2', mvp_v1_2);
+
+app.use('/vision-v1', vision_v1);
 
 app.use('/emails', emails);
 
