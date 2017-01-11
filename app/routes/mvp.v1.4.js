5e31962e2735366efb59bb54a39bbf3cb907fa70
Demotive <mat@demotive.com>
2017-01-11 16:06:49 +0000

    Move address to after DOB

--- app/routes/mvp.v1.4.js
+++ app/routes/mvp.v1.4.js
@@ -83,9 +83,134 @@ router.post('/date-of-birth', function (req, res) {
     if (req.session.edit === true) {
       res.redirect('confirm-details')
     } else {
+      res.redirect('home-address')
+    }
+  }
+})
+
+// Postcode ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+router.get('/home-address', function (req, res) {
+  res.render('mvp_v1_4/home-address-postcode');
+});
+
+router.post('/home-address', function (req, res) {
+
+  if (!req.session.homeAddress) {
+    req.session.homeAddress = {}
+  }
+
+  req.session.homeAddress.postcode = req.body['postcode'];
+  req.session.homeAddress.building = req.body['building'];
+
+  if (req.body['postcode'] === '') {
+    res.render('mvp_v1_4/home-address-postcode', {
+      error: {
+        postcode: 'Please enter your home postcode'
+      }
+    });
+  } else {
+    // strip spaces
+    var cleaned = req.session.homeAddress.postcode.replace(/\s+/g, '').toLowerCase();
+
+    request('https://api.getAddress.io/v2/uk/' + cleaned + '/?api-key=' + postcode_api + '&format=true', function (error, response, body) {
+      if (!error) {
+        if (response.statusCode == 200) {
+          var parsed = JSON.parse(body);
+          var addresses = parsed['Addresses'];
+          addresses.sort(naturalSort);
+          var filtered = [];
+
+          if (req.session.homeAddress.building !== '') {
+            for (var i=0; i<addresses.length; i++) {
+              //var current = addresses[i][0];
+              var current = addresses[i].toString().toLowerCase();
+              if (current.indexOf(req.session.homeAddress.building.toLowerCase()) !== -1) {
+                filtered.push(addresses[i]);
+              }
+            }
+
+            if (filtered.length === 0) {
+              // Nothing found for this combo of building / postcode
+              // So just display the postcode results?
+              req.session.addressResults = addresses;
+              res.render('mvp_v1_4/home-address-result', {
+                message: 'No exact match has been found, showing all addresses for ' + req.session.homeAddress.postcode,
+              });
+            } else {
+              req.session.addressResults = filtered;
+              res.render('mvp_v1_4/home-address-result');
+            }
+
+          } else {
+
+            req.session.addressResults = addresses;
+
+            res.render('mvp_v1_4/home-address-result');
+
+          }
+        }
+
+      } else {
+        res.render('mvp_v1_4/home-address-postcode', {
+          error: {
+            general: 'Sorry, there’s been a problem looking up your address. Please try again.'
+          }
+        });
+      }
+    });
+  }
+})
+
+// Address selection +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+router.get('/select-address', function (req, res) {
+  res.render('mvp_v1_4/select-address');
+});
+
+router.post('/select-address', function (req, res) {
+
+  if (!req.body['address']) {
+    res.render('mvp_v1_4/home-address-result', {
+      error: 'Please select your home address'
+    });
+  } else {
+    req.session.homeAddress.address = req.body['address'].split(',');
+    if (req.session.edit === true) {
+      res.redirect('confirm-details')
+    } else {
       res.redirect('nhs-number')
     }
   }
+})
+
+// Manual address entry ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+router.get('/home-address-manual', function (req, res) {
+  res.render('mvp_v1_4/home-address-manual');
+});
+
+router.post('/home-address-manual', function (req, res) {
+
+  if (!req.session.homeAddress) {
+    req.session.homeAddress = {}
+  }
+
+  req.session.homeAddress.address = [
+    req.body['address-1'],
+    req.body['address-2'],
+    req.body['address-3'],
+    req.body['address-4']
+  ];
+  req.session.homeAddress.postcode = req.body['postcode'];
+
+  if (!req.body['address-1'] && !req.body['address-4']) {
+    res.render('mvp_v1_4/home-address-manual', {
+      error: 'Please enter your full address'
+    });
+  } else if (req.session.edit === true) {
+    res.redirect('confirm-details')
+  } else {
+    res.redirect('nhs-number')
+  }
+
 })
 
 // NHS Number ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -192,133 +317,8 @@ router.post('/contact-telephone', function (req, res) {
   if (req.session.edit === true) {
     res.redirect('confirm-details')
   } else {
-    res.redirect('home-address')
-  }
-})
-
-// Postcode ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-router.get('/home-address', function (req, res) {
-  res.render('mvp_v1_4/home-address-postcode');
-});
-
-router.post('/home-address', function (req, res) {
-
-  if (!req.session.homeAddress) {
-    req.session.homeAddress = {}
-  }
-
-  req.session.homeAddress.postcode = req.body['postcode'];
-  req.session.homeAddress.building = req.body['building'];
-
-  if (req.body['postcode'] === '') {
-    res.render('mvp_v1_4/home-address-postcode', {
-      error: {
-        postcode: 'Please enter your home postcode'
-      }
-    });
-  } else {
-    // strip spaces
-    var cleaned = req.session.homeAddress.postcode.replace(/\s+/g, '').toLowerCase();
-
-    request('https://api.getAddress.io/v2/uk/' + cleaned + '/?api-key=' + postcode_api + '&format=true', function (error, response, body) {
-      if (!error) {
-        if (response.statusCode == 200) {
-          var parsed = JSON.parse(body);
-          var addresses = parsed['Addresses'];
-          addresses.sort(naturalSort);
-          var filtered = [];
-
-          if (req.session.homeAddress.building !== '') {
-            for (var i=0; i<addresses.length; i++) {
-              //var current = addresses[i][0];
-              var current = addresses[i].toString().toLowerCase();
-              if (current.indexOf(req.session.homeAddress.building.toLowerCase()) !== -1) {
-                filtered.push(addresses[i]);
-              }
-            }
-
-            if (filtered.length === 0) {
-              // Nothing found for this combo of building / postcode
-              // So just display the postcode results?
-              req.session.addressResults = addresses;
-              res.render('mvp_v1_4/home-address-result', {
-                message: 'No exact match has been found, showing all addresses for ' + req.session.homeAddress.postcode,
-              });
-            } else {
-              req.session.addressResults = filtered;
-              res.render('mvp_v1_4/home-address-result');
-            }
-
-          } else {
-
-            req.session.addressResults = addresses;
-
-            res.render('mvp_v1_4/home-address-result');
-
-          }
-        }
-
-      } else {
-        res.render('mvp_v1_4/home-address-postcode', {
-          error: {
-            general: 'Sorry, there’s been a problem looking up your address. Please try again.'
-          }
-        });
-      }
-    });
-  }
-})
-
-// Address selection +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-router.get('/select-address', function (req, res) {
-  res.render('mvp_v1_4/select-address');
-});
-
-router.post('/select-address', function (req, res) {
-
-  if (!req.body['address']) {
-    res.render('mvp_v1_4/home-address-result', {
-      error: 'Please select your home address'
-    });
-  } else {
-    req.session.homeAddress.address = req.body['address'].split(',');
-    if (req.session.edit === true) {
-      res.redirect('confirm-details')
-    } else {
-      res.redirect('current-gp')
-    }
-  }
-})
-
-// Manual address entry ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-router.get('/home-address-manual', function (req, res) {
-  res.render('mvp_v1_4/home-address-manual');
-});
-
-router.post('/home-address-manual', function (req, res) {
-
-  if (!req.session.homeAddress) {
-    req.session.homeAddress = {}
-  }
-
-  req.session.homeAddress.address = [
-    req.body['address-1'],
-    req.body['address-2'],
-    req.body['address-3'],
-    req.body['address-4']
-  ];
-  req.session.homeAddress.postcode = req.body['postcode'];
-
-  if (!req.body['address-1'] && !req.body['address-4']) {
-    res.render('mvp_v1_4/home-address-manual', {
-      error: 'Please enter your full address'
-    });
-  } else if (req.session.edit === true) {
-    res.redirect('confirm-details')
-  } else {
     res.redirect('current-gp')
   }
-
 })
 
 // Current GP ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
